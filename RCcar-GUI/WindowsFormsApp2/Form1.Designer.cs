35679db1500515664ec4383f6b6858cf4b29af6e
Aliaa Mohamed <102418786+aliaxema@users.noreply.github.com>
2023-02-02 23:44:28 +0200

    Add files via upload

---
+++ RCcar-GUI/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(560, 14);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(570, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 135);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Discounnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(571, 51);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(384, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Disconnected";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button4.ForeColor = System.Drawing.Color.Blue;
+            this.button4.Location = new System.Drawing.Point(150, 77);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 54);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Forward";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button5.ForeColor = System.Drawing.Color.Blue;
+            this.button5.Location = new System.Drawing.Point(378, -93);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 37);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Right";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button6.ForeColor = System.Drawing.Color.Blue;
+            this.button6.Location = new System.Drawing.Point(150, 218);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(103, 54);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Down";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button7.ForeColor = System.Drawing.Color.Blue;
+            this.button7.Location = new System.Drawing.Point(30, 147);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 54);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Left";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(134, 316);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(151, 45);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Automatic";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Tahoma", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(134, 16);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(154, 45);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "Manual";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button10.ForeColor = System.Drawing.Color.Red;
+            this.button10.Location = new System.Drawing.Point(150, 154);
+            this.button10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(103, 41);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Stop";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button11.Location = new System.Drawing.Point(333, 319);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(151, 42);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "Distance";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button12.ForeColor = System.Drawing.Color.Blue;
+            this.button12.Location = new System.Drawing.Point(271, 147);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(103, 54);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "Right";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Fax", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(427, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 23);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(528, 319);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(151, 42);
+            this.button13.TabIndex = 21;
+            this.button13.Text = "Clear";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(691, 416);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ RCcar-GUI/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = null;
+        string distanse;
+        string dataRX = " ";
+        delegate void serialCalback(string val);
+        public Form1()
+        {
+            InitializeComponent();
+            refreshCom();
+            label1.Text = "Disconnected";
+            label1.ForeColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            refreshCom();
+        }
+        private void refreshCom()
+        {
+            comboBox1.DataSource = SerialPort.GetPortNames();
+        }
+        private void Connect()
+        {
+            port = new SerialPort(comboBox1.SelectedItem.ToString());
+            port.BaudRate = 9600;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                    label1.Text = "Connected";
+                    label1.ForeColor = Color.Green;
+                        }
+            }catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+
+
+        }
+        private void Disconnect()
+        {
+            try
+            {
+                if (port.IsOpen)
+                {
+                    port.Close();
+                    label1.Text = "Disconnected";
+                    label1.ForeColor = Color.Red;
+                }
+            }catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Disconnect();
+        }
+
+       
+
+        /*private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text =  trackBar1.Value.ToString();
+           
+        }
+        */
+      
+        /*private void trackBar1_MouseLeave(object sender, EventArgs e)
+        {
+            try
+            {
+                port.Write(trackBar1.Value.ToString());
+            }catch(Exception ex) { }
+        }*/
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                port.Write("A");
+            }
+            catch (Exception ex) { }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                port.Write("M");
+            }
+            catch (Exception ex) { }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                port.Write("S");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+
+         }
+
+        private void up()
+        {
+            try
+            {
+                port.Write("2");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void back()
+        {
+            try
+            {
+                port.Write("4");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void right()
+        {
+            try
+            {
+               
+                port.Write("6");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void left()
+        {
+            try
+            {
+                port.Write("8");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            up();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            left();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            back();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            right();
+        }
+
+       
+        private void button11_Click(object sender, EventArgs e)
+        {
+            DIStance();
+            textBox1.Text = dataRX;
+            
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            right();
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                string temp = sp.ReadExisting();
+                //setText(temp);
+                dataRX += temp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                 
+            }
+        }
+       /* private void setText(string val)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                serialCalback scb = new serialCalback(setText);
+                this.Invoke(scb, new object[] { val });
+
+            }
+            else
+            {
+                distanse = val;
+
+            }
+        }*/
+        private void distance()
+        {
+            try
+            {
+                port.Write("D");
+                // distanse = port.ReadExisting();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void DIStance()
+        {
+            try
+            {
+                port.Write("D");
+            }
+            catch
+            (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            dataRX = " ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
